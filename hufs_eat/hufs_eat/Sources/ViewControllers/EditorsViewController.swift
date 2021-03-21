03b8e01daa67a14ba7059a1e5912da2c113ad869
jeongyeob97 <leejeongyeob97@gmail.com>
2021-03-22 00:57:14 +0900

    [#2] ㅋcollectionview 간격 조정

--- hufs_eat/hufs_eat/Sources/ViewControllers/EditorsViewController.swift
+++ hufs_eat/hufs_eat/Sources/ViewControllers/EditorsViewController.swift
@@ -71,5 +71,13 @@ extension EditorsViewController: UICollectionViewDelegateFlowLayout {
         
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
 }
 
